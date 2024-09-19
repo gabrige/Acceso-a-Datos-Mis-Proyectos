bab3a650d6eb96a41e62003c4a9b08a8897b88a5
Gabriel <gabrige16@gmail.com>
2024-09-19 21:29:28 +0200

    commit

--- Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
+++ Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
@@ -8,13 +8,20 @@ public class Entrada {
     /*Parte 1: Listar el nombre de todos los ficheros ubicados en un directorio (en clase hemos puesto como ejemplo un
     directorio creado en el paquete resources)*/
 
-        
 
 
 
 
 
+    /*Parte 2: Crear un subdirectorio dentro de la carpeta anterior y listar el nombre de todos los archivos*/
 
+
+
+
+
+    /*Parte 3: Listar el nombre de todos los ficheros y directorios de una ruta concreta, como por ejemplo C:/Users
+    Para la parte 3 tenéis que utilizar el concepto de recursividad. Investigad a que se refiere ese término y en la clase del jueves lo trataremos,
+    así como la resolución del ejercicio.*/
 
 
 
