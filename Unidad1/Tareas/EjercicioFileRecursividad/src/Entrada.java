8201f0637bc164386a5e128eec9cade25ed23227
Gabriel <gabrige16@gmail.com>
2024-09-23 16:51:13 +0200

    push

--- Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
+++ Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
@@ -1,29 +1,29 @@
+import controller.GestorFicheros;
+
 import java.io.File;
 
 public class Entrada {
     public static void main(String[] args) {
 
-    File fichero1 = new File("C:\\Users\\gabri\\Documents\\FP DAM UNIR\\Segundo Curso\\Acceso A Datos\\Proyectos IntelliJ IDEA\\Acceso-a-Datos---UNIR\\Unidad1\\Tareas\\EjercicioFileRecursividad\\src\\resources\\directorio");
+        GestorFicheros gestorFicheros = new GestorFicheros();
 
     /*Parte 1: Listar el nombre de todos los ficheros ubicados en un directorio (en clase hemos puesto como ejemplo un
     directorio creado en el paquete resources)*/
 
-    
-
-
-
+        System.out.println("Part 1");
+        gestorFicheros.lecturaDirectorios("src/resources/directorio");
 
     /*Parte 2: Crear un subdirectorio dentro de la carpeta anterior y listar el nombre de todos los archivos*/
 
-
-
-
+        System.out.println("\n" + "Part 2");
+        gestorFicheros.listarNombres("src/resources/directorio");
 
     /*Parte 3: Listar el nombre de todos los ficheros y directorios de una ruta concreta, como por ejemplo C:/Users
     Para la parte 3 tenéis que utilizar el concepto de recursividad. Investigad a que se refiere ese término y en la clase del jueves lo trataremos,
     así como la resolución del ejercicio.*/
 
-
+        System.out.println("\n" + "Part 3");
+        gestorFicheros.lecturaRecursiva("C:\\Users\\gabri\\Documents\\FP DAM UNIR\\Segundo Curso\\Acceso A Datos\\Proyectos IntelliJ IDEA\\Acceso-a-Datos---UNIR\\Unidad1\\Fichero");
 
     }
 }
--- Unidad1/Tareas/EjercicioFileRecursividad/src/controller/GestorFicheros.java
+++ Unidad1/Tareas/EjercicioFileRecursividad/src/controller/GestorFicheros.java
@@ -1,4 +1,55 @@
 package controller;
 
+import java.io.File;
+
 public class GestorFicheros {
+
+    public void lecturaDirectorios(String path){
+        File file = new File(path);
+        File[] ficheros = file.listFiles();
+        for(File item : ficheros){
+            if(!item.isHidden()){
+                System.out.println(item.getName());
+            }
+        }
+
+    }
+
+    public void listarNombres(String path){
+        File file = new File(path);
+        File[] ficheros = file.listFiles();
+        for(File item : ficheros){
+            System.out.println(item.getName());
+            if(item.isDirectory()){
+                File[] subdirectorio = item.listFiles();
+                for(File file1 : subdirectorio){
+                    System.out.println("\t"+file1.getName());
+                }
+            }
+        }
+    }
+
+    public void lecturaRecursiva(String path) {
+        File file = new File(path); // paso a un fichero logico y fisico
+        //File[] ficheros = file.listFiles();
+        for (File item : file.listFiles()) {
+            System.out.println(item.getName());
+            if (item.isDirectory()) {
+                lecturaSubdirectorios(item);
+            }
+            // pregunto si es directorio
+            // saco todos los ficheros del directorio y los muestro -> NO SE CUANTAS VECES PREGUNTO
+        }
+    }
+
+    // RECURSIVIDAD -> EJECUCION QUE SE LLAMA A ELLA MISMA, CON CUIDADO TENGA UNA SALIDA
+    private void lecturaSubdirectorios(File fichero) {
+        for (File file : fichero.listFiles()) {
+            System.out.println("\t" + file.getName());
+            if (file.isDirectory()) {
+                lecturaSubdirectorios(file);
+            }
+        }
+    }
+
 }
