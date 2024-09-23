859ace9babefe8d1bea8370a8a2c4cb193e9f56c
Gabriel <gabrige16@gmail.com>
2024-09-23 13:14:00 +0200

    push

--- Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
+++ Unidad1/Tareas/EjercicioFileRecursividad/src/Entrada.java
@@ -3,12 +3,12 @@ import java.io.File;
 public class Entrada {
     public static void main(String[] args) {
 
-        File fichero1 = new File("C:\\Users\\gabri\\Documents\\FP DAM UNIR\\Segundo Curso\\Acceso A Datos\\Proyectos IntelliJ IDEA\\Acceso-a-Datos---UNIR\\Unidad1\\Tareas\\EjercicioFileRecursividad\\src\\resources\\directorio");
+    File fichero1 = new File("C:\\Users\\gabri\\Documents\\FP DAM UNIR\\Segundo Curso\\Acceso A Datos\\Proyectos IntelliJ IDEA\\Acceso-a-Datos---UNIR\\Unidad1\\Tareas\\EjercicioFileRecursividad\\src\\resources\\directorio");
 
     /*Parte 1: Listar el nombre de todos los ficheros ubicados en un directorio (en clase hemos puesto como ejemplo un
     directorio creado en el paquete resources)*/
 
-
+    
 
 
 
