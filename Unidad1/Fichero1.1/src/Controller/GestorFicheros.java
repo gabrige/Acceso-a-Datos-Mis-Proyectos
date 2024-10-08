9c55a8669e6405faa5be766326366b769dac67aa
Gabriel <gabrige16@gmail.com>
2024-10-08 12:43:52 +0200

    commit

--- Unidad1/Fichero1.1/src/Controller/GestorFicheros.java
+++ Unidad1/Fichero1.1/src/Controller/GestorFicheros.java
@@ -15,6 +15,61 @@ public class GestorFicheros {
 
                   try{
                       fileReader = new FileReader(file);
+                      //fileReader.read(); //Para quitar este error poner una "clause(catch)" con alt + enter
+                      // el .read() lee una letra por letra
+                      int lectura = 0;
+
+                      while((lectura = fileReader.read()) > -1){
+
+                          System.out.print((char) lectura);
+
+                      }
+
+                  } catch (FileNotFoundException e) {
+
+                      System.out.println("Fallo en la lectura del Fichero");
+                      System.out.println(e.getMessage());
+
+                  } catch (IOException e) {
+                      throw new RuntimeException(e);
+                  } finally {
+
+                      try {
+                          fileReader.close();
+
+                      } catch (IOException | NullPointerException e) {
+
+                          System.out.println("Error del cerrado del flujo");
+
+                      } catch(Exception e){
+
+                          System.out.println("Error en el cerrado por ser nulo");
+
+                      }
+
+                  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              /* 1ºForma De Hacer La Excepcion
+
+              try{
+                      fileReader = new FileReader(file);
                   } catch (FileNotFoundException e) {
                       System.out.println("Fallo en la lectura del Fichero");
                       System.out.println(e.getMessage());
@@ -24,16 +79,34 @@ public class GestorFicheros {
                       } catch (IOException | NullPointerException e) { // as í tratamos las excepciones especificas que queramos
                           System.out.println("Error del cerrado del flujo");
                       }
-                      /*
+
                       si añadimos seguido otro catch(Exception){
 
                             esto hará que si no encontrado ninguna de aquellas excepciones pero encuentra cualquier otra excepcion no especifica se cierre
 
                         }
-                      */
 
-                  }
 
-                }
+                   }*/
+
+
+                  /* 2ºForma De Hacer La Excepcion
+
+                  fileReader = new FileReader(file); // Alt + Enter y añadimos excepcion "throws FileNotFoundException" en lel void
+
+                   Luego en el metodo main
+
+                   try{
+                        gestorFicheros.lecturaTextoPlano(src / Resources / Lectura.txt);
+
+                    } catch (FileNotFoundException) {
+
+                    }
+
+                   Esto se hace para tratar una excepcion de manera general y no tan especifica como la primera
+                   */
+
+              }
+
     }
 }
--- Unidad1/Fichero1.1/src/Entrada.java
+++ Unidad1/Fichero1.1/src/Entrada.java
@@ -1,6 +1,20 @@
+import Controller.GestorFicheros;
+
+import java.io.FileNotFoundException;
+
+
 public class Entrada {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+        GestorFicheros gestorFicheros = new GestorFicheros();
+
+
+        gestorFicheros.lecturaTextoPlano("src/Resources/Lectura.txt");
+        /*
+        Ultimo recurso para tratar la excepcion "Alt + Enter y añadir en el main throws FileNotFoundException"
+        Deberíamos solo usar la primera forma
+        */
 
 
 
