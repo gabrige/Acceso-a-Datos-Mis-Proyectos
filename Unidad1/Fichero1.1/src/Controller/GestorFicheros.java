f011b58ac10b9f9909b8f1c3a8f6132889cfd379
Gabriel <gabrige16@gmail.com>
2025-02-04 20:12:14 +0100

    c

--- Unidad1/Fichero1.1/src/Controller/GestorFicheros.java
+++ Unidad1/Fichero1.1/src/Controller/GestorFicheros.java
@@ -5,127 +5,64 @@ import java.io.*;
 public class GestorFicheros {
 
     public void lecturaTextoPlano(String path){
+        // FILE -> FILEREADER -> BUFFEREDREADER  -> cuando se terminan SE CIERRAN
         File file = new File(path);
         //FileReader fileReader = null;
         BufferedReader bufferedReader = null;
 
-              if(file.exists() && file.isFile()){
+        if (file.exists() && file.isFile()) {
+            // existe y ademas es un fichero
+            // trato la excepcion aqui
+            try {
+                //fileReader = new FileReader(file);
+                bufferedReader = new BufferedReader(new FileReader(file));
+                /* LECTURA CON FILEREADER
+                int lectura=0;
+                // mientras exista el numero haz la lectura e imprime
+                while ((lectura = fileReader.read())!=-1){
+                    // imprime
+                    System.out.print((char) lectura);
+                }*/
+                String lectura = null;
+                // String lecturaCompleta = null;
+                // StringBuffer lecturaCompleta = new StringBuffer(); -> sincronizado (si tengo varios hilos, se
+                // cominucan entre ellos para decir quien tiene el objeto ocupado
+                StringBuilder lecturaCompleta = new StringBuilder(); // solo lo va a utilizar un hilo
+                while ((lectura = bufferedReader.readLine())!=null){
+                    //System.out.println(lectura);
+                    //lecturaCompleta+=lectura;
+                    lecturaCompleta.append(lectura);
+                    lecturaCompleta.append("\n");
+                }
+                System.out.println(lecturaCompleta.toString());
+                // JSON
+                // toda la lectura
+                /*int lectura = fileReader.read(); // int -> UNICODE ASCI
+                System.out.print((char) lectura);
+                lectura = fileReader.read(); // int -> UNICODE ASCI
+                System.out.print((char) lectura);
+                lectura = fileReader.read(); // int -> UNICODE ASCI
+                System.out.print((char) lectura);*/
 
-                  try{
-                      //fileReader = new FileReader(file);
-                      //fileReader.read(); //Para quitar este error poner una "clause(catch)" con alt + enter
-                      // el .read() lee una letra por letra
-
-                      //Forma de lectura secuencial completa con "FileReader"
-                      /* int lectura = 0;
-
-                      while((lectura = fileReader.read()) > -1){
-
-                          System.out.print((char) lectura);
-
-                      }*/
-
-                      /*
-                      bufferedReader = new BufferedReader(new FileReader(file)); -> es sincronizado (si tiene varios hilos, se comunican
-                      entre ellos para decir quien tiene el objeto ocupado)
-
-                      String lectura = null;
-                      while((lectura = bufferedReader.readLine()) != null){
-
-                          System.out.println(lectura);
-
-                      }
-
-                       */
-
-                      StringBuilder lecturaCompleta = new StringBuilder(); //solo lo va a utilizar un hilo
-                      // es hacer el mismo while pero adaptado al string builder
-                      while((lectura = bufferedReader.readLine()) != null){
-
-                          System.out.println(lectura);
-
-                      }
-
-                  } catch (FileNotFoundException e) {
-
-                      System.out.println("Fallo en la lectura del Fichero");
-                      System.out.println(e.getMessage());
-
-                  } finally {
-
-                      try {
-                          fileReader.close();
-
-                      } catch (IOException | NullPointerException e) {
-
-                          System.out.println("Error del cerrado del flujo");
-
-                      } catch(Exception e){
-
-                          System.out.println("Error en el cerrado por ser nulo");
-
-                      }
-
-                  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-              /* 1ºForma De Hacer La Excepcion
-
-              try{
-                      fileReader = new FileReader(file);
-                  } catch (FileNotFoundException e) {
-                      System.out.println("Fallo en la lectura del Fichero");
-                      System.out.println(e.getMessage());
-                  } finally {
-                      try {
-                          fileReader.close();
-                      } catch (IOException | NullPointerException e) { // as í tratamos las excepciones especificas que queramos
-                          System.out.println("Error del cerrado del flujo");
-                      }
-
-                      si añadimos seguido otro catch(Exception){
-
-                            esto hará que si no encontrado ninguna de aquellas excepciones pero encuentra cualquier otra excepcion no especifica se cierre
-
-                        }
-
-
-                   }*/
-
-
-                  /* 2ºForma De Hacer La Excepcion
-
-                  fileReader = new FileReader(file); // Alt + Enter y añadimos excepcion "throws FileNotFoundException" en lel void
-
-                   Luego en el metodo main
-
-                   try{
-                        gestorFicheros.lecturaTextoPlano(src / Resources / Lectura.txt);
-
-                    } catch (FileNotFoundException) {
-
-                    }
-
-                   Esto se hace para tratar una excepcion de manera general y no tan especifica como la primera
-                   */
-
-              }
+            } catch (FileNotFoundException e) {
+                System.out.println("Fallo en la lectura");
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Error en la escritura, fallo de permisos");
+            } finally {
+                // se ejecuta SI o SI
+                try {
+                    bufferedReader.close(); // NULLPOINTEREXCEPTION
+                } catch (IOException e) {
+                    System.out.println("Error en el cerrado del flujo");
+                    System.out.println(e.getMessage());
+                    // por favor introduce una ruta valida
+                } catch (Exception e) {
+                    System.out.println("Error en el cerrado por ser nulo");
+                }
+            }
+        }
 
     }
+
 }
